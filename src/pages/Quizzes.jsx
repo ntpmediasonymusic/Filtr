aae2d71a709fe1ba9b5a948f20661a057f54391a
Oscar Gonzalez Esparza <ogonzalez@hermescorp.io>
2025-02-07 10:47:07 -0600

    [ADD] QuizzesPreview

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import Quiz from "../components/Quiz";
 import useSortedQuizzes from "../hooks/useSortedQuizzes";
 
 const Quizzes = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const sortedQuizzes = useSortedQuizzes();
   return (
     <div className="flex flex-col gap-[50px] md:gap-[100px] px-6 py-[50px] md:py-[100px]">
